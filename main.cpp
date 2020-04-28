99cd38a234a1893fa8d43c3fcb0c6f9eb1b6b6b4
olupelumi <pelumifafowora@gmail.com>
2020-04-28 19:01:58 -0400

    commits not working?

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 KSEQ_INIT(int, read)
 
 
+int min3(int x, int y, int z);
+static int leven_distance(string str1, string str2, int m, int n);
+
 int main() {
 
     LSH *lsh = new LSH();
@@ -57,4 +60,50 @@ int main() {
     fclose(fp);
     return 0;
     
+}
+
+int min3(x, y, z) 
+{ 
+    return min(min(x, y), z); 
+} 
+/*
+* Requires:
+*   str1 and str2 are both genomic sequences represented as strings
+*   m is the index of the character we're looking at in str1
+*   n is the index of the character we're looking at in str2
+*
+* Effect:
+*   Computes the Levenshtein distance between the two strings(the minimum number of changes to turn str1 into str2)
+*   returns the levenshtein distance as an integer
+*/
+
+int leven_distance(str1, str2, m, n)
+{
+    //dynamic programming
+    //Initializing my dp matrix
+    int dp[m + 1][n + 1]; //an extra box for when looking at the empty string prefix of either str1 or str2 or both
+
+    //filling matrix
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++){
+            //if i =0, first string is empty, we need to insert all the characters of string 2 to string 1
+            if (i == 0)
+                dp[i][j] = j;
+
+            //if j =0, first string is empty, we need to insert all the characters of string 2 to string 1
+            if (j == 0)
+                dp[i][j] = i;
+
+            //if the previous character of str1 and str2 are equal, 
+            else if (str1[i-1] == str2[j-1])//remember that the dp[i][j] represents the distance between str1 up to index i-1 and str2 up to index j-1
+                dp[i][j] = dp[i-1][j-1];
+            
+            // if the previous characters are different, consider all the possibilities
+            else
+                dp[i][j] = 1 + min3(dp[i-1][j-1], //replace
+                                    dp[i-1][j], //remove
+                                    dp[i][j-1]); //insert
+        }
+    }
+    return dp[m][n];
 }
