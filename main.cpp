ba21f95ab94c0587d860176713606c20faad178c
olupelumi <pelumifafowora@gmail.com>
2020-04-29 18:39:58 -0400

    got my distance metric to work properly. Also made so that we don't have to always insert the size of the strings

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ KSEQ_INIT(int, read)
 
 
 static int min3(int x, int y, int z);
-static int leven_distance(std::string str1, std::string str2, int m, int n);
+static int leven_distance(std::string str1, std::string str2);
 
 int main() {
 
     LSH *lsh = new LSH();
+
 
     unsigned int i[] = {1, 1, 1, 2, 2, 2, 3, 3, 3};
 
@@ -34,7 +35,10 @@ int main() {
         std::cout << r[i] << "\n";
     }
     
-    
+    int test_edit;
+
+    test_edit = leven_distance("oluwapelumi","back");
+    printf("should say 10: %d \n", test_edit); 
     FILE* fp;
     kseq_t *seq;
     int n = 0, slen = 0, qlen = 0;
@@ -54,7 +58,7 @@ int main() {
                 min = cur;
         }
         //std::cout << "\n Above is put together \n" << str_seq << "\n";
-        std::cout << std::hash<std::string>{}(str_seq) << " " << min << "\n";
+        //std::cout << std::hash<std::string>{}(str_seq) << " " << min << "\n";
     }
     printf("%d\t%d\t%d\n", n, slen, qlen);
     kseq_destroy(seq);
@@ -68,6 +72,7 @@ min3(int x, int y, int z)
 { 
     return std::min(std::min(x, y), z); 
 } 
+
 /*
 * Requires:
 *   str1 and str2 are both genomic sequences represented as strings
@@ -79,33 +84,45 @@ min3(int x, int y, int z)
 *   returns the levenshtein distance as an integer
 */
 
-int leven_distance(std::string str1, std::string str2, int m, int n)
+int leven_distance(std::string str1, std::string str2)
 {
     //dynamic programming with storing
+    int m;
+    int n;
+    m = (int)strlen(str1.c_str());
+    n = (int)strlen(str2.c_str());
+    std::cout << "me: " << m << "\n";
     //Initializing my dp matrix
+   
+    printf("m,n: %d,%d\n", m, n);
     int dp[m + 1][n + 1]; //an extra box for when looking at the empty string prefix of either str1 or str2 or both
 
     //filling matrix
     for (int i = 0; i <= m; i++) {
         for (int j = 0; j <= n; j++){
             //if i =0, first string is empty, we need to insert all the characters of string 2 to string 1
-            if (i == 0)
+            if (i == 0) {
+                printf("i is 0 \n");
                 dp[i][j] = j;
-
+                continue;
+            }
             //if j =0, first string is empty, we need to insert all the characters of string 2 to string 1
-            if (j == 0)
+            if (j == 0) {
                 dp[i][j] = i;
-
+                continue;
+            }
             //if the previous character of str1 and str2 are equal, 
-            else if (str1[i-1] == str2[j-1])//remember that the dp[i][j] represents the distance between str1 up to index i-1 and str2 up to index j-1
+            if (str1[i-1] == str2[j-1]) {//remember that the dp[i][j] represents the distance between str1 up to index i-1 and str2 up to index j-1
                 dp[i][j] = dp[i-1][j-1];
-            
+                continue;
+            }
             // if the previous characters are different, consider all the possibilities
-            else
-                dp[i][j] = 1 + min3(dp[i-1][j-1], //replace
+            dp[i][j] = 1 + min3(dp[i-1][j-1], //replace
                                     dp[i-1][j], //remove
                                     dp[i][j-1]); //insert
         }
     }
     return (dp[m][n]);
 }
+
+//Thinking about all the comparisons we'll have to do, we may need to store the information in an array 
